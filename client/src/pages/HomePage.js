94091d2008ee66cecdc526e15f6055ce20d2edbc
Dheeraj Kumar <55842638+Dheeraj12121@users.noreply.github.com>
2023-10-05 21:02:31 +0530

    filter functions

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,83 +1,124 @@
-import React, { useState , useEffect,  } from "react";
-import { Form, Modal, Select, Table, message } from "antd";
+import React, { useState, useEffect } from "react";
+import { Form, Modal, Select, Table, message, DatePicker } from "antd";
 import Layout from "../components/Layout/Layout";
 import axios from "axios";
-import Spin from "../components/Spin"
+import Spin from "../components/Spin";
+import moment from 'moment';
+import { RangePicker } from " DatePicker";
 
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [getAllTransection, setGetAllTransection] = useState([])
+  const [allTransection, setAllTransection] = useState([]);
+  const [frequency, setFrequency] = useState("7");
+  const [selectedDate, setSelectedDate] = useState([]);
+  const [type, setType] = useState('all')
 
   // table data
   const columns = [
     {
-      title:'Date',
-      dataIndex:'date'
+      title: "Date",
+      dataIndex: "date",
+      render: (text) => <span>{moment(text).format('YYYY-MM-DD')}</span>
     },
     {
-      title:'Amount',
-      dataIndex:'amount'
+      title: "Amount",
+      dataIndex: "amount",
     },
     {
-      title:'Type',
-      dataIndex:'type'
+      title: "Type",
+      dataIndex: "type",
     },
     {
-      title:'Category',
-      dataIndex:'category'
+      title: "Category",
+      dataIndex: "category",
     },
     {
-      title:'Refrence',
-      dataIndex:'refrence'
+      title: "Refrence",
+      dataIndex: "refrence",
     },
     {
-      title:'Actions',
+      title: "Actions",
     },
-  ]
-
+  ];
 
   // getall Transection
-  const getAllTransactions = async () => {
-    try {
-      const user = JSON.parse(localStorage.getItem('user'));
-      setLoading(true)
-      await axios.post('/transections/get-transection',{userid:user._id})
-      setLoading(false)
-      setGetAllTransection(res.data)
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-      message.error("Ftech Issue With Trancton")
-    }
-  };
 
   // useEffect Hook
   useEffect(() => {
-    getAllTransactions(); 
-  }, [])
+    const getAllTransactions = async () => {
+      try {
+        const user = JSON.parse(localStorage.getItem("user"));
+        setLoading(true);
+        const res = await axios.post("/transections/get-transection", {
+          userid: user._id,
+          frequency,
+          selectedDate,
+          type
+        });
+        setLoading(false);
+        setAllTransection(res.data);
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+        message.error("Ftech Issue With Trancton");
+      }
+    };
+    getAllTransactions();
+  }, [frequency, selectedDate, type]);
 
   // form handling
   const handleSubmit = async (value) => {
     try {
-      const user = JSON.parse(localStorage.getItem('user'))
-      setLoading(true)
-      await axios.post('/transections/add-transection', {...values,userid:user.id})
-      setLoading(false)
-      message("Transection Added Successfully")
-      setShowModal(false)
+      const user = JSON.parse(localStorage.getItem("user"));
+      setLoading(true);
+      await axios.post("/transections/add-transection", {
+        ...values,
+        userid: user.id,
+      });
+      setLoading(false);
+      message("Transection Added Successfully");
+      setShowModal(false);
     } catch (error) {
-      setLoading(false)
-      message.error("Faild to add transection")
+      setLoading(false);
+      message.error("Faild to add transection");
     }
   };
 
   return (
     <>
       <Layout>
-      {loading && <Spin/>}
+        {loading && <Spin />}
         <div className="filters"></div>
-        <div>rangefilters</div>
+        <div>
+          <h6>Select Frequency</h6>
+          <Select value={frequency} onChange={(values) => setFrequency(values)}>
+            <Select.Option value="7">Last 1 Week</Select.Option>
+            <Select.Option value="30">Last 1 Month</Select.Option>
+            <Select.Option value="365">Last 1 Year</Select.Option>
+            <Select.Option value="custom">Custom</Select.Option>
+          </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(values) => setSelectedDate(values)}
+            />
+          )}
+        </div>
+        <div>
+          <h6>Select Type</h6>
+          <Select value={type} onChange={(values) => setType(values)}>
+            <Select.Option value="all">ALL</Select.Option>
+            <Select.Option value="income">INCOME</Select.Option>
+            <Select.Option value="expense">EXPENSE</Select.Option>
+          </Select>
+          {frequency === "custom" && (
+            <RangePicker
+              value={selectedDate}
+              onChange={(values) => setSelectedDate(values)}
+            />
+          )}
+        </div>
         <div>
           <button
             className="btn btn-primary"
@@ -87,7 +128,7 @@ const HomePage = () => {
           </button>
         </div>
         <div className="content">
-          <Table columns={colums} dataSource={allTransection}/>
+          <Table columns={colums} dataSource={allTransection} />
         </div>
         <Modal
           title="Add Transection"
--- controllers/transectionCtrl.js
+++ controllers/transectionCtrl.js
@@ -1,23 +1,37 @@
-
-const transectionModel = require('../models/transectionModel');
-const getAllTransection = async (req, res) =>{
-    try {
-        const transections = await transectionModel.find({userid:req.body.userid})
-        res.status(200).json(transections);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json(error)
-    }
-}
+const transectionModel = require("../models/transectionModel");
+const moment = require("moment");
+const getAllTransection = async (req, res) => {
+  try {
+    const { frequency, selectedDate ,type } = req.body;
+    const transections = await transectionModel.find({
+      ...(frequency !== 'custom' ? {
+        date: {
+            $gt: moment().subtract(Number(frequency), "d").toDate(),
+          },
+      } : {
+        date:{
+            $get: selectedDate[0],
+            $lte: selectedDate[1],
+        }
+      } ),
+      userid: req.body.userid,
+      ...(type !== 'all' && {type})
+    });
+    res.status(200).json(transections);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
 const addTransection = async () => {
-    try{
-        const newTransection = new transectionModel(req.body)
-        await newTransection.save()
-        res.status(201).send('Transection Created')
-    }catch(error){
-        console.log(error);
-        res.status(500).json(error)
-    }
+  try {
+    const newTransection = new transectionModel(req.body);
+    await newTransection.save();
+    res.status(201).send("Transection Created");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 };
 
-module.exprots = {getAllTransection, addTransection};
+module.exprots = { getAllTransection, addTransection };
